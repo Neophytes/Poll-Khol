de76e689bffc1af3e712e580e9998fbf3f0b5010
jig08 <grover.jigyasa1@gmail.com>
2015-08-22 02:02:54 +0530

    Trial Twitter login

---
+++ app/src/main/java/com/example/jigyasa/poll_khol/ConnectionDetector.java
@@ -0,0 +1,37 @@
+package com.example.jigyasa.poll_khol;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by jigyasa on 22/8/15.
+ */
+public class ConnectionDetector {
+
+    private Context _context;
+
+    public ConnectionDetector(Context context){
+        this._context = context;
+    }
+
+    /**
+     * Checking for all possible internet providers
+     * **/
+    public boolean isConnectingToInternet(){
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null)
+        {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+
+}
